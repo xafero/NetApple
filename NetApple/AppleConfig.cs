1fe42c5e0ff29d58f5b03e932cb235897a6823ff
xafero <georg.berendt@uni-oldenburg.de>
2017-07-19 22:32:29 +0200

    First initial code to generate a rock-ridge Apple-ish DMG file on Windows

---
+++ NetApple/AppleConfig.cs
@@ -0,0 +1,10 @@
+﻿
+namespace NetApple
+{
+    public class AppleConfig
+    {
+        public string BuildDirectory { get; set; }
+        public string BundleName { get; set; }
+        public string DiskImageFile { get; set; }
+    }
+}
---
+++ NetApple/Program.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NetApple
+{
+    public static class Program
+    {
+        public static int Main(string[] parms)
+        {
+            if (parms.Length != 1)
+            {
+                Console.WriteLine("Usage: [json-file]");
+                return -1;
+            }
+            var set = new JsonSerializerSettings();
+            var text = File.ReadAllText(parms.First(), Encoding.UTF8);
+            var config = JsonConvert.DeserializeObject<AppleConfig>(text, set);
+            var exe = Path.GetFullPath("mkisofs.exe");
+            var args = new List<string>();
+            args.Add("-V");
+            args.Add(config.BundleName);
+            args.Add("-D");
+            args.Add("-R");
+            args.Add("-apple");
+            args.Add("-no-pad");
+            args.Add("-o");
+            args.Add(config.DiskImageFile);
+            args.Add(config.BuildDirectory);
+            var info = new ProcessStartInfo
+            {
+                FileName = exe,
+                Arguments = string.Join(" ", args),
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false
+            };
+            using (var proc = Process.Start(info))
+            {
+                if (proc == null)
+                    throw new NotImplementedException(info.FileName);
+                proc.OutputDataReceived += Proc_OutputDataReceived;
+                proc.ErrorDataReceived += Proc_ErrorDataReceived;
+                proc.BeginOutputReadLine();
+                proc.BeginErrorReadLine();
+                proc.WaitForExit();
+                return proc.ExitCode;
+            }
+        }
+
+        static void Proc_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.Error.WriteLine(e.Data);
+        }
+
+        static void Proc_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.Out.WriteLine(e.Data);
+        }
+    }
+}
